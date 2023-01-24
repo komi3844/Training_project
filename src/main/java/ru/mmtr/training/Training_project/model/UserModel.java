f1091c54cce0e38b8f6b45505444a705e3cbbbc7
komi3844 <47776516+komi3844@users.noreply.github.com>
2023-01-24 16:26:55 +0300

    update controller and add model

--- src/main/java/ru/mmtr/training/Training_project/model/UserModel.java
+++ src/main/java/ru/mmtr/training/Training_project/model/UserModel.java
@@ -3,30 +3,24 @@ package ru.mmtr.training.Training_project.model;
 import io.swagger.annotations.ApiModelProperty;
 
 public class UserModel {
-    @ApiModelProperty(notes = "Provided user name", required =true)
-    private String player;
-
-    @ApiModelProperty(notes = "The system generated greeting message" , readOnly =true)
-    private String message;
-
-    public UserModel(String player, String message) {
-        this.player = player;
-        this.message = message;
+    int id;
+    String name;
+    public int getId() {
+        return id;
     }
 
-    public String getPlayer() {
-        return player;
+    public void setId(int id) {
+        this.id = id;
     }
 
-    public void setPlayer(String player) {
-        this.player = player;
+    public String getName() {
+        return name;
     }
 
-    public String getMessage() {
-        return message;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public void setMessage(String message) {
-        this.message = message;
-    }
+
+
 }
